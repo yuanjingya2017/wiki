1932b02658a1ac13615597d8bf5bbe6dd1f31b7c
Lshuming <33562319+Lshuming@users.noreply.github.com>
2018-10-17 18:51:26 +0800

    five tips for JS (#3)
    * vue组件通信
    
    * 修改错字
    
    * ruter->router
    
    * modify
    
    * five tips for JS

--- config/li.js
+++ config/li.js
@@ -2,7 +2,8 @@ module.exports = {
   sidebar: {
     title: '李书明',
     children: [
-      ['/docs/li/vueChat', 'Vue的组件通信']
+      ['/docs/li/vueChat', 'Vue的组件通信'],
+      ['/docs/li/fiveTipsForJS', 'JS编写条件语句的五个技巧']
     ]
   }
 }
