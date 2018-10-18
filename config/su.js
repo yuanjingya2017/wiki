99d6cc63ec25c109d9e2ff86975255ec62cf3d1d
Su <hesu@newbanker.cn>
2018-10-18 20:15:44 +0800

    完成防抖和节流

--- config/su.js
+++ config/su.js
@@ -2,7 +2,8 @@ module.exports = {
   sidebar: {
     title: '苏鹤',
     children: [
-      ['/docs/su/markdown', 'markdown语法入门']
+      ['/docs/su/markdown', 'markdown语法入门'],
+      ['/docs/su/debounceAndThrottle', '函数防抖与函数节流']
     ]
   }
 }
